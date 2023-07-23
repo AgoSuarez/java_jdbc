bca341e75c4d14e82a3facd0e5ea590b4e41a491
AgoSuarez <agosuaort@gmail.com>
2023-07-23 10:44:50 +0100

    Create class Person

---
+++ Example2JDBC/src/main/java/domain/Person.java
@@ -0,0 +1,81 @@
+package domain;
+
+//called ENTITY
+public class Person {
+    private int idPerson;
+    private String name;
+    private String lastName;
+    private String email;
+    private String phone;
+
+    public Person() {
+    }
+
+    public Person(int idPerson){
+        this.idPerson=idPerson;
+    }
+
+    public Person(String name, String lastName, String email, String phone) {
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+    public Person(int idPerson, String name, String lastName, String email, String phone) {
+        this.idPerson = idPerson;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.phone = phone;
+    }
+    
+
+    public int getIdPerson() {
+        return idPerson;
+    }
+
+    public void setIdPerson(int idPerson) {
+        this.idPerson = idPerson;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "idPerson=" + idPerson + ", name=" + name + ", lastName=" + lastName + ", email=" + email + ", phone=" + phone + '}';
+    }
+    
+    
+    
+}
